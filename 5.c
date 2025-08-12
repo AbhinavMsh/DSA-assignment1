9586589e1ab26b619ce14098183473d72fab3aba
Abhinav Masih <chinu23official@gmail.com>
2025-08-12 11:50:44 +0530

    added 5.c

---
+++ 5.c
@@ -0,0 +1,18 @@
+// check if prime or not
+#include <stdio.h>
+int main(){
+    int a, factors = 1;
+    scanf("%d",&a);
+    for(int i=1;i<a;i++){
+        if (a % i == 0) {
+            factors++;
+        }
+    }
+    if (factors > 2){
+        printf("Is not prime");
+    }
+    else{
+        printf("Is prime");
+    }
+    return 0;
+}
