4dd1a876090528907da260faee902c8e4aad1aca
Abhinav Masih <chinu23official@gmail.com>
2025-08-12 11:33:23 +0530

    4.c

---
+++ 4.c
@@ -0,0 +1,16 @@
+// sum of First N numbers
+#include <stdio.h>
+
+int main(){
+    int a=1,sum=0;
+
+    printf("Enter num:");
+    scanf("%d",&a);
+
+    for (;a>0;a--){
+        sum = sum + a;
+
+    }
+    printf("%d", sum);
+    return 0;
+}
